c786041a206e7dc768b6157b28ed1da8189b6f24
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2022-07-14 16:06:55 -0400

    Streamlit it.

--- calgen.py
+++ calgen.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import re
 import pandas as pd
 from ics import Calendar, Event
@@ -19,19 +20,6 @@ special_dates = [
 special_dates = pd.DataFrame(special_dates, columns=["date", "desc", "pattern"])
 special_dates['date'] = pd.to_datetime(special_dates['date'])
 
-# Read the input file.
-data = pd.read_excel('/Users/ka37/Dropbox/Mac/Downloads/View_My_Teaching_Schedule.xlsx')
-
-# Merge shadow reservations (multiple locations for the same course section and time)
-data['Location'] = data.groupby(['Course Section', 'Meeting Time'])['Location'].transform(lambda x: ', '.join(x))
-data = data.drop_duplicates(['Course Section', 'Meeting Time'])
-
-# Parse the "meeting time" field.
-parsed = pd.concat([data, data['Meeting Time'].str.extract(r'^(?P<days>\w+) \| (?P<time>[^|]+)')], axis=1)
-
-# Use single letters for each date ("R" instead of "TH" for Thursday)
-parsed['days'] = parsed['days'].str.replace('TH', 'R')
-
 
 def iter_meeting_dates(start_date: date, end_date: date, pattern: str, special_dates):
     '''Yield all meeting times for the given class, given a meeting pattern.'''
@@ -49,6 +37,7 @@ def iter_meeting_dates(start_date: date, end_date: date, pattern: str, special_d
             break
         cur += one_day
 
+
 def parse_time(x):
     """Parse 1:00 PM into hour=13, min=0."""
     hour, min, meridian = re.match(r'^(\d+):(\d+) (AM|PM)', x).groups()
@@ -59,24 +48,47 @@ def parse_time(x):
     return dict(hour=hour, minute=min)
 
 
-cal = Calendar()
-for i in range(len(data)):
-    start_time, end_time = parsed['time'].iloc[i].split(' - ')
-    start_time_p = parse_time(start_time)
-    end_time_p = parse_time(end_time)
-    for meeting_date in iter_meeting_dates(
-        parsed['Start Date'].iloc[i].date(),
-        parsed['End Date'].iloc[i].date(),
-        parsed['days'].iloc[i],
-        special_dates
-    ):
-        evt = Event()
-        evt.name = data['Course Section'].iloc[i]
-        evt.begin = pd.Timestamp(meeting_date).replace(**start_time_p).tz_localize('US/Eastern')
-        evt.end = pd.Timestamp(meeting_date).replace(**end_time_p).tz_localize('US/Eastern')
-        evt.location = data['Location'].iloc[i]
-        cal.events.add(evt)
+
+st.title("Teaching Schedule Converter")
+uploaded_file = st.file_uploader("Select the Teaching Schedule Excel file exported from Workday.")
+
+if uploaded_file is not None:
+    # Read the input file.
+    data = pd.read_excel(uploaded_file)
+
+    # Merge shadow reservations (multiple locations for the same course section and time)
+    data['Location'] = data.groupby(['Course Section', 'Meeting Time'])['Location'].transform(lambda x: ', '.join(x))
+    data = data.drop_duplicates(['Course Section', 'Meeting Time'])
+
+    # Parse the "meeting time" field.
+    parsed = pd.concat([data, data['Meeting Time'].str.extract(r'^(?P<days>\w+) \| (?P<time>[^|]+)')], axis=1)
+
+    # Use single letters for each date ("R" instead of "TH" for Thursday)
+    parsed['days'] = parsed['days'].str.replace('TH', 'R')
 
 
+    cal = Calendar()
+    for i in range(len(data)):
+        start_time, end_time = parsed['time'].iloc[i].split(' - ')
+        start_time_p = parse_time(start_time)
+        end_time_p = parse_time(end_time)
+        for meeting_date in iter_meeting_dates(
+            parsed['Start Date'].iloc[i].date(),
+            parsed['End Date'].iloc[i].date(),
+            parsed['days'].iloc[i],
+            special_dates
+        ):
+            evt = Event()
+            evt.name = data['Course Section'].iloc[i]
+            evt.begin = pd.Timestamp(meeting_date).replace(**start_time_p).tz_localize('US/Eastern')
+            evt.end = pd.Timestamp(meeting_date).replace(**end_time_p).tz_localize('US/Eastern')
+            evt.location = data['Location'].iloc[i]
+            cal.events.add(evt)
 
-open('out.ics', 'w').write(cal.serialize())
+
+    st.download_button(
+        label="Download .ics file",
+        data=cal.serialize(),
+        file_name="fall_2022_teaching.ics",
+        mime="text/calendar"
+    )
